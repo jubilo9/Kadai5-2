1611fe7c8baf9229c5a09c45ff28caaa800380ce
mayuko soeta <mayuko soeta@192.168.192.173>
2015-05-09 00:20:39 +0900

    0~999 ドライバー；jubilo9　ナビゲーター：yuki0202

--- src/junit/eng/IntToEng.java
+++ src/junit/eng/IntToEng.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class IntToEng {
 
 
-	    // ���C�����\�b�h
+	    // �ｿｽ�ｿｽ�ｿｽC�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ\�ｿｽb�ｿｽh
 	    public static void main(String[] args) {
 
 	        Scanner sc = new Scanner(System.in);
@@ -14,25 +14,31 @@ public class IntToEng {
 
 	    }
 
-	    // ���l���p�󂷂�ϊ����郁�\�b�h
+	    // �ｿｽ�ｿｽ�ｿｽl�ｿｽ�ｿｽ�ｿｽp�ｿｽｷゑｿｽﾏ奇ｿｽ�ｿｽ�ｿｽ�ｿｽ驛�ｿｽ\�ｿｽb�ｿｽh
 	    static String translateEng(int n) {
 	    	String []eng1={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twenteen","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nighteen"};
 	    	String []eng2={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
 	    	String ans = "";
-	    	String ans2 = "";
+	    	//String ans2 = "";
+	    	
 	    	if(n<20) return eng1[n];
+	    	if(n%100==0) return eng1[n/100]+" hundred";
 	    	else {
 	    		String c = String.valueOf(n);
 	    		int l = c.length();
 	    		int a = Integer.parseInt(c.substring(l-2,l));
 	    		int nu = 0;
-	    		if(a<20) ans2=eng1[a];
+	    		if(a<20) ans=eng1[a];
 	    		else {
-	    			ans = eng2[Integer.parseInt(String.valueOf(c.charAt(0)))];
-	    			nu = Integer.parseInt(String.valueOf(c.charAt(1))) ;
+	    			String a2 =  String.valueOf(a);
+	    			ans = eng2[Integer.parseInt(String.valueOf(a2.charAt(0)))];
+	    			nu = Integer.parseInt(String.valueOf(a2.charAt(1))) ;
 	    			if(nu > 0){
 	    				ans += " " + eng1[nu];
 	    			};
+	    		}
+	    		if(n>100){
+	    			ans = eng1[n/100] +" hundred "+ans;
 	    		}
 	    		return ans;
 	    	}
--- test/junit/eng/IntToEngTest.java
+++ test/junit/eng/IntToEngTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class IntToEngTest {
 
 	@Test
-	public void test() {
+	public void test3() {
 		IntToEng ite = new IntToEng();
 		String expected = "three";
 		String actual = ite.translateEng(3);
@@ -15,10 +15,31 @@ public class IntToEngTest {
 		
 	}
 	@Test
-	public void test20(){
+	public void test21(){
 		IntToEng  ite = new IntToEng();
 		String expected = "twenty one";
 		String actual = ite.translateEng(21);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test100(){
+		IntToEng  ite = new IntToEng();
+		String expected = "one hundred";
+		String actual = ite.translateEng(100);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test915(){
+		IntToEng  ite = new IntToEng();
+		String expected = "nine hundred fifteen";
+		String actual = ite.translateEng(915);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test987(){
+		IntToEng  ite = new IntToEng();
+		String expected = "nine hundred eighty seven";
+		String actual = ite.translateEng(987);
 		assertEquals(actual,expected);
 	}
 }
