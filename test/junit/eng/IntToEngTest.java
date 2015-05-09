a577d3520528f4f3a2f474194524335b79279982
kimuraYuki <kimuraYuki@172.19.2.112>
2015-05-09 22:27:18 +0900

    ドライバー　yuki0202 ナビゲーターjubilo9 1000〜9999追加しました

--- test/junit/eng/IntToEngTest.java
+++ test/junit/eng/IntToEngTest.java
@@ -42,4 +42,47 @@ public class IntToEngTest {
 		String actual = ite.translateEng(987);
 		assertEquals(actual,expected);
 	}
+	@Test
+	public void test1000(){
+		IntToEng  ite = new IntToEng();
+		String expected = "one thousand";
+		String actual = ite.translateEng(1000);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test2345(){
+		IntToEng  ite = new IntToEng();
+		String expected = "two thousand three hundred forty five";
+		String actual = ite.translateEng(2345);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test3412(){
+		IntToEng  ite = new IntToEng();
+		String expected = "three thousand four hundred twelve";
+		String actual = ite.translateEng(3412);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test3602(){
+		IntToEng  ite = new IntToEng();
+		String expected = "three thousand six hundred two";
+		String actual = ite.translateEng(3602);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test3440(){
+		IntToEng  ite = new IntToEng();
+		String expected = "three thousand four hundred forty";
+		String actual = ite.translateEng(3440);
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void test3012(){
+		IntToEng  ite = new IntToEng();
+		String expected = "three thousand twelve";
+		String actual = ite.translateEng(3012);
+		assertEquals(actual,expected);
+	}
+	
 }
